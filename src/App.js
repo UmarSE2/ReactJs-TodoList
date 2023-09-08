703beb72f3675aea46a4cfb9bccb2d4b11ec8abc
UmarSE2 <umer.se02@gmail.com>
2023-09-08 14:59:45 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { Heading, IconButton, VStack, useColorMode, useToast } from "@chakra-ui/react";
+import { FaSun, FaMoon } from "react-icons/fa";
+import TaskList from "./components/tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
+  const toast = useToast();
+  const [tasks, setTasks] = useState(
+    () => JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  function deleteTask(id) {
+    const newTasks = tasks.filter((task) => {
+      return task.id !== id;
+    });
+    setTasks(newTasks);
+  }
+
+  function deleteTaskAll() {
+    setTasks([]);
+  }
+
+  function checkTask(id) {
+    const newTasksCheck = tasks.map((task, index, array) => {
+      if (task.id === id) {
+        task.check = !task.check;
+      }
+      return task;
+    });
+    setTasks(newTasksCheck);
+  }
+
+  function updateTask(id, body, onClose) {
+    const info = body.trim();
+
+    if (!info) {
+      toast({
+        title: "Enter your task",
+        position: "top",
+        status: "warning",
+        duration: 2000,
+        isClosable: true,
+      });
+
+      return;
+    }
+
+    const newTasksUpdate = tasks.map((task, index, array) => {
+      if (task.id === id) {
+        task.body = body;
+        task.check = false;
+      }
+      return task;
+    });
+
+    setTasks(newTasksUpdate);
+
+    onClose();
+  }
+
+  function addTask(task) {
+    setTasks([...tasks, task]);
+  }
+
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VStack p={4} minH='100vh' pb={28}>
+      <IconButton
+        icon={colorMode === "light" ? <FaSun /> : <FaMoon />}
+        isRound='true'
+        size='md'
+        alignSelf='flex-end'
+        onClick={toggleColorMode}
+        aria-label='toogle-dark-mode'
+      />
+
+      <Heading
+        p='5'
+        fontWeight='extrabold'
+        size='xl'
+        bgGradient='linear(to-r, red.500, yellow.500)'
+        bgClip='text'
+      >
+        Todo list
+      </Heading>
+      <AddTask addTask={addTask} />
+      <TaskList
+        tasks={tasks}
+        updateTask={updateTask}
+        deleteTask={deleteTask}
+        deleteTaskAll={deleteTaskAll}
+        checkTask={checkTask}
+      />
+    </VStack>
   );
 }
 
